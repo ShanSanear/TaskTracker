54fcb3141acbf6df25d94bd3cb20ca823a9e62ca
Shan <pruskimateusz93@gmail.com>
2022-10-22 12:05:01 +0200

    Initial Docker integration handling

--- src/api/main.py
+++ src/api/main.py
@@ -1,19 +1,8 @@
-import uvicorn as uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+api = FastAPI()
 
 
-@app.get("/")
+@api.get("/")
 def hello():
-    return {"message": "Hello!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "api.main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True,
-        debug=True,
-    )
+    return {"message": "Hello world!"}
