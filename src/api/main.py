7bfe37fff918d25d0358894389073937e9d7e559
Shan <pruskimateusz93@gmail.com>
2022-10-25 17:55:47 +0200

    Added database example code

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
 
+from common.database.database_engine import User, database
+
 api = FastAPI()
 
 
 @api.get("/")
 def hello():
     return {"message": "Hello world!"}
+
+
+@api.get("/users")
+async def read_root():
+    return await User.objects.all()
+
+
+@api.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@api.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
---
+++ src/common/database/database_engine.py
@@ -0,0 +1,26 @@
+import databases
+import ormar
+import sqlalchemy
+
+from common.database.config import settings
+
+database = databases.Database(settings.db_string)
+metadata = sqlalchemy.MetaData()
+
+
+class BaseMeta(ormar.ModelMeta):
+    metadata = metadata
+    database = database
+
+
+class User(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "users"
+
+    id: int = ormar.Integer(primary_key=True)
+    email: str = ormar.String(max_length=128, unique=True, nullable=False)
+    active: bool = ormar.Boolean(default=True, nullable=False)
+
+
+engine = sqlalchemy.create_engine(settings.db_string)
+metadata.create_all(engine)
