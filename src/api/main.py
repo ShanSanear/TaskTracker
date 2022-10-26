b689107247f35976a28172ae976ce06d23860192
Shan <pruskimateusz93@gmail.com>
2022-10-26 21:21:39 +0200

    Added code handling changes in database handling structure

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from common.database.database_engine import engine, SessionLocal
 
-from common.database.database_engine import User, database
+from common.database import schemas, models, crud
 
 api = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @api.get("/")
@@ -10,20 +20,22 @@ def hello():
     return {"message": "Hello world!"}
 
 
-@api.get("/users")
-async def read_root():
-    return await User.objects.all()
+@api.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 
-@api.on_event("startup")
-async def startup():
-    if not database.is_connected:
-        await database.connect()
-    # create a dummy entry
-    await User.objects.get_or_create(email="test@test.com")
+@api.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
-
-@api.on_event("shutdown")
-async def shutdown():
-    if database.is_connected:
-        await database.disconnect()
+@api.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
