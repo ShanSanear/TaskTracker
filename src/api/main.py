6829d8247940d0079b9920f4dfd95c6cb1e2c75a
Shan <pruskimateusz93@gmail.com>
2022-11-01 13:38:54 +0100

    Code now in line with usage of ormar library example

--- src/api/main.py
+++ src/api/main.py
@@ -1,43 +1,25 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from common.database.database_engine import engine, SessionLocal
+from fastapi import FastAPI
 
-from common.database import schemas, models, crud
+from common.database.models import User, database
 
 api = FastAPI()
-models.Base.metadata.create_all(bind=engine) # TODO, remove this after using alembic for migration purposes
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 @api.get("/")
 def hello():
     return {"message": "Hello world!"}
 
+@api.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    await database.connect()
+    await User.objects.get_or_create(name="Name", email="TestEmail@domain.com")
 
-@api.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+@api.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
 
-
-@api.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@api.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+@api.get("/users/")
+async def read_users():
+    return await User.objects.all()
