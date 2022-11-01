9abf56c7b4e6aae9c00ff9eda2a0fd7696309011
Shan <pruskimateusz93@gmail.com>
2022-11-01 13:35:02 +0100

    Added usage of db_string being taken from environment variables from Docker

--- src/common/database/config.py
+++ src/common/database/config.py
@@ -1,10 +1,20 @@
+import logging
+from pprint import pprint
 
 from dynaconf import Dynaconf
+logging.basicConfig(level=logging.DEBUG)
+from pathlib import Path
 
 settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=['settings.toml', '.secrets.toml'],
+    settings_files=[Path(__file__).parent / "settings.toml"],
+    envvar_prefix=False,
 )
 
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
-# `settings_files` = Load these files in the order.
+def get_db_string_from_settings():
+    pprint(list(sorted(settings.items(), key=lambda x: x[0])))
+    print(f"Settings: {settings.items()}", flush=True)
+    return f"{settings.DB_ENGINE}://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@" \
+           f"{settings.DB_HOSTNAME}:{settings.DB_PORT}/{settings.POSTGRES_DB}"
+
+
+settings.db_string = get_db_string_from_settings()
